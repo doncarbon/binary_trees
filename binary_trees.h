1770c63ea2a3e39976a14d67bc50d19dea2ca3f7
Hamza Yassine <ihamzayassine@gmail.com>
2024-03-26 22:26:29 +0000

    Task 0 done

--- binary_trees.h
+++ binary_trees.h
@@ -16,19 +16,20 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 
+/* Tasks */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
